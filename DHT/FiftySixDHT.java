dd2914f3758362eea4f6d8468b54d4f8318e5a6a
Jonathan <j10lam@ryerson.ca>
2019-04-07 18:31:18 -0400

    Modified main DHT

--- DHT/FiftySixDHT.java
+++ DHT/FiftySixDHT.java
@@ -5,12 +5,13 @@ import javax.ws.rs.core.Response.Status; // used to return status codes
 
 public class FiftySixDHT implements Serializable
 {
-    private List<Node> nodes = new ArrayList<Node>();
+    private List<Node> nodes;
     private int port;
 
     public FiftySixDHt()
     {
       port = 56;
+      nodes = new ArrayList<Node>();
       instantiateNodes();
       linkNodes();
     }
@@ -18,24 +19,47 @@ public class FiftySixDHT implements Serializable
     public void instantiateNodes()
     {
         int num_nodes = 4;
-        for (int i = 0; i < num_nodes; i++)
+        for (int i = 1; i <= num_nodes; i++)
         {
-            int id = i;
-            //set up nodes;
-
-
+            InetAddress addr = InetAddress.getByName("127.0.0." + Integer.toString(i));
+            Node n = new Node(i, addr, port);
+            nodes.add(n);
         }
     }
 
     public void linkNodes()
     {
-
+        for (int i = 0; i < this.nodes.size(); i++)
+        {
+            if (i == 0)
+            {
+                this.nodes.linkNodes(this.nodes.get(3), this.nodes.get(1));
+            }
+            else
+            {
+                this.nodes.linkNodes(this.nodes.get(i-1), this.nodes.get(i+1));
+            }
+        }
     }
 
     // if no content found must return HTTP response codes
-    public Node findNode()
+    public Node findNode(String content_name)
     {
+        Node n;
+        int target = modHash(content_name);
+        for (int i = 0; i < this.nodes.size(); i++)
+        {
+            if (this.nodes.get(i).id == target)
+            {
+                n = this.nodes.get(i);
+                return n;
+            }
+        }
 
+        if (n == null)
+        {
+            // send HTTP response 404
+        }
     }
 
     // write file found in node to a serializable form
@@ -49,11 +73,4 @@ public class FiftySixDHT implements Serializable
     {
 
     }
-
-
-
-
-
-
-
 }
