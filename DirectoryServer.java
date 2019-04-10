d8b59b88d790199b9b15ed1a99d48b61a53444a2
Jonathan <j10lam@ryerson.ca>
2019-04-09 23:15:21 -0400

    Replaced DS

--- DirectoryServer.java
+++ DirectoryServer.java
@@ -1,5 +1,7 @@
 import java.util.*;
 import java.lang.*;
+import java.net.*;
+import java.io.*;
 import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
@@ -9,202 +11,255 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+
+public class TCPServer
+{
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWrite out;
+    private BufferedReader in;
+    private String ipList;
+    private String clientID;
+    private DirectoryServer directoryServer;
 
 
-//What does node need to contain?
+    public TCPServer(int port, DirectoryServer ds)
+    {
+        this.directoryServer = ds;
+        this.serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+    }
 
-//Param:
-//  ID (2^n-1)
-//  UDP port for P2P Client (init, get, update, exit)
-//  TCP port for Node-Node Communication
-//  Child node
-//  Parent node
-//  IP
-//  Table (content name, IP)
+    public void start()
+    {
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamreader(clientSocket.getInputStream()));
+        String greeting = in.readLine();
 
+        if ("hello server".equals(greeting))
+        {
+            out.println("hello client");
+        }
+        else
+        {
+            out.println("unrecognized greeting");
+        }
 
-//Functions
-//  Constructor (grabs IP) x
-//  linkNodes x
-//  Store content IP (read buffer) x
-//  return content IP (write buffer) x
-//  create client socket
-//  create server socket
-//  main function (instantiate the nodes)
+    }
 
+    public void stop()
+    {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
 
-public class DirectoryServer extends Thread {
+//    public static void main(String[] args)
+//    {
+//        DSTCPSrv server = new DSTCPSrv(20650);
+//        server.start();
+//    }
+}
 
-    private int id;
-    private int ip;
-    private int udpPort;
-    private int tcpPort;
-    private String predecessor;
-    private String sucessor;
-    private Map<String, Integer> data;
-    private String primaryNode;
+public class TCPClient {
+    private Socket clientSocket;
+    private PrinterWriter out;
+    private BufferedReader in;
+    private String targetID;
+    private String ipList;
+    private DirectoryServer directoryServer;
 
-    //UDP - Client-DS
+    public TCPClient(String ip, int port, DirectoryServer ds)
+    {
+        this.targetID = targetID;
+        this.ipList = ipList;
+        clientSocket = new Socket(ip, port);
+        this.directoryServer = ds;
+    }
+
+    public void start(String ip, int port) {
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+    }
+
+    public String sendMessage(String msg) {
+        out.println(msg);
+        String resp = in.readLine();
+        return resp;
+    }
+
+    public void stop() {
+        in.close();
+        out.close();
+        clientSocket.close();
+    }
+}
+
+//    @Test
+//    public void givenGreetingClient_whenServerRespondsWhenStarted_thenCorrect() {
+//        DSTCPCli client = new DSTCPCli();
+//        client.startConnection("127.0.0.1", 20650);
+//        String response = client.sendMessage("hello server");
+//        assertEquals("hello client", response);
+//    }
+
+class UDPServer extends Thread
+{
     private DatagramSocket socket;
     private boolean running;
     private byte[] buffer = new byte[256];
+    DirectoryServer directoryServer;
 
-    //TCP - DS-DS - Server
-    DatagramSocket serverSocket = new DatagramSocket(9876); //temp port
-    byte[] receiveData = new byte[1024];
-    byte[] sendData = new byte[1024];
 
-    //TCP - DS-DS - Client
-    DatagramSocket clientSocket = new DatagramSocket();
-    InetAddress IPAddress = InetAddress.getByName("Noahs-MacBook-Pro.local");
-    boolean serverOn = true;s
-
-    public DirectoryServer(int id) {
-        this.id = id;
-        //this.ip = ;
-        this.udpPort = 20650;
-        this.tcpPort = 20650;
-
-        //Setup UDP for Client-DS
-        this.socket = new DatagramSocket(this.udpPort);
-    }
-
-    public void linkDS(String child, String parent) {
-        this.predecessor = child;
-        this.sucessor = parent;
-    }
-
-    public void storeData(String contentName, int clientIP) {
-        int reponseCode = 200;
-        this.data.put(contentName, clientIP);
-
-        //do something with responseCode
-    }
-
-    public int getData(String contentName) {
-        int clientIp = this.data.get(contentName);
-        if (clientIp != -1) {
-            return clientIp;
-        } else {
-            return 404; // change to HTTP Response Codes
-        }
-    }
-
-    // UDP for Client-DS
-    public void run() {
+    public UDPServer(int port, DirectoryServer ds);
+    {
+        buffer = new byte[256];
+        socket = new DatagramSocket(port);
+        directoryServer = ds;
         running = true;
+    }
 
-        byte[] sendData = new byte[1024];
-        byte[] receiveData = new byte[1024];
-        
-        while (running) {
+    public void run()
+    {
+        while(running)
+        {
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
             socket.receive(packet);
 
-            InetAddress address = packet.getAddress();
             int port = packet.getPort();
-            packet = new DatagramPacket(receiveData, receiveData.length, address, port); // return message
+            InetAddress address = packet.getAddress();
 
-            String request = new String(packet.getData(), 0, packet.getLength());
+            packet = new DatagramPacket(buffer, buffer.length, address, port);
+            String msg = new String(packet.getData(), 0, packet.getLength());
 
-            // evaluate request messages //init, get, update, exit
-            switch (request) {
-                case "init":
-                    break;
-
-                case "update":
-                    break;
-
-                case "query":
-                    break;
-                case "exit":
-                    running = false;
-                    continue;
-                    break;
-                default:
-                    //throw exception
+            if (msg.equals("end"))
+            {
+                running = false;
+                continue;
             }
-
-
             socket.send(packet);
         }
         socket.close();
     }
 
-    // TCP for DS-DS-Server
-    public void runServerTCP() {
-        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-        serverSocket.receive(receivePacket);
-        String sentence = new String(receivePacket.getData());
-
-        InetAddress IPAddress = receivePacket.getAddress();
-
-        int port = receivePacket.getPort();
-
-        //String capitalizedSentence = sentence.toUpperCase();
-        //sendData = capitalizedSentence.getBytes();
-        String code = "200";
-        sendData = code.getBytes();
-
-        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-
-        serverSocket.send(sendPacket);
-    }
-
-    // TCP for DS-DS-Client
-    public void runClientTCP() {
-        while(serverOn)
-        {
-
-            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-
-            byte[] sendData = new byte[1024];
-            byte[] receiveData = new byte[1024];
-
-            String sentence = inFromUser.readLine();
-            sendData = sentence.getBytes();
-
-            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
-            clientSocket.send(sendPacket);
-
-            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
-            clientSocket.receive(receivePacket);
-
-            String result = new String(receivePacket.getData());
-
-            System.out.println("FROM SERVER:" + result);
-
-            if(sentence.toUpperCase().equals("EXIT"))
-            {
-                serverOn = false;
-            }
-
-        }
-        clientSocket.close();
-
-    }
- 
-    public static void main(String args[])
+    public void stop()
     {
-        //Instantiate variables
-        Scanner scanner = New Scanner(System.in);
-        int id = Integer.parseInt(args[0]);
-        DirectoryServer t = new DirectoryServer(id);
+        socket.close();
+    }
+}
 
-        //Get Machine IP
-        InetAddress ip = InetAddress.getLocalHost();
-        String address = ip.getHostAddress(); //ip address of machine
-        System.out.println("Dir. Server - ID: " + id + " IP Address: " + address);
+public class DirectoryServer {
+    private String ip;
+    private int id;
+    private int port;
+    private Map<String, List<String>> data;
+    private String prev;
+    private String next;
+    private String primaryDS;
 
-        //Set DS ID#1 IP
-        System.out.println("Enter DS #1 IP: ");
-        t.primaryNode = in.nextLine();
+    private UDPServer udpServer;
+    private TCPClient tcpClient;
+    private TCPServer tcpServer;
 
-        //Set Neighbor IPs
-        System.out.println("Enter Child IP (space) Parent ID: ");
-        String[] neighbors = scanner.nextLine().split("\\s+");
-        t.linkDS(neighbors[0], neighbors[1]);
+    private String ipList;
 
-        scanner.close();
+    public DirectoryServer(int id, int port) {
+        this.ip = InetAddress.getLocalHost().getHostAddress();
+        this.id = id;
+        this.port = port;
+
+        this.data = new HashMap<String, List<String>>();
+
+        startTCPServer();
+        startUDPServer();
+    }
+
+    public void startUDPServer() {
+        udpServer = new UDPServer(port, this);
+        udpServer.start();
+    }
+
+    /** TCPServer should start TCP Client to send **/
+    public void startTCPServer() {
+        TCPServer tcpServer = new TCPServer(port, this);
+        tcpServer.start();
+    }
+
+    /** Only used for Node #1 instantiated by TCPServer **/
+    public String getIPS() {
+        tcpClient = new TCPClient(ip, port, this);
+        tcpClient.start();
+        // TCPServer will set the value of ipList
+    }
+
+    public void setIPS(String msg) {
+        ipList = msg;
+    }
+
+    public void setPrimary(String primaryNode) {
+        primaryDS = primaryNode;
+    }
+
+    public void setNeighbors(String prev, String next) {
+        this.prev = prev;
+        this.next = next;
+    }
+
+    public String getPrimaryDS() {
+        return primaryDS;
+    }
+
+    public List<String> getNeighbors {
+        List<String> neighbors = new ArrayList<String>();
+        neighbors.add(this.prev);
+        neighbors.add(this.next);
+
+        return neighbors;
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public String getIP() {
+        return ip;
+    }
+
+    public String getPrimaryDS() {
+        return primaryDS;
+    }
+
+    public void setData(String contentName, String location) {
+        List<String> storedValues = data.get(contentName);
+
+        if(storedValues != null) {
+            storedValues.add(location);
+        }
+        else {
+            storedValues = new ArrayList<String>();
+            storedValues.add(location);
+        }
+
+        data.put(contentName, storedValues);
+    }
+
+    public List<String> getData(String contentName) {
+        return data.get(contentName);
+        /** Add NULL Check **/
+    }
+
+    public boolean removeData(String contentName, String location) {
+        List<String> storedValues = data.get(contentName);
+
+        if(storedValues != null && storedValues.contains(location)) {
+            storedValues.remove(location);
+            data.put(contentName, storedValues);
+            return true;
+        }
+
+        return false;
     }
 }
---
+++ DirectoryServerOLD.java
@@ -0,0 +1,210 @@
+import java.util.*;
+import java.lang.*;
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+//What does node need to contain?
+
+//Param:
+//  ID (2^n-1)
+//  UDP port for P2P Client (init, get, update, exit)
+//  TCP port for Node-Node Communication
+//  Child node
+//  Parent node
+//  IP
+//  Table (content name, IP)
+
+
+//Functions
+//  Constructor (grabs IP) x
+//  linkNodes x
+//  Store content IP (read buffer) x
+//  return content IP (write buffer) x
+//  create client socket
+//  create server socket
+//  main function (instantiate the nodes)
+
+
+public class DirectoryServer extends Thread {
+
+    private int id;
+    private int ip;
+    private int udpPort;
+    private int tcpPort;
+    private String predecessor;
+    private String sucessor;
+    private Map<String, Integer> data;
+    private String primaryNode;
+
+    //UDP - Client-DS
+    private DatagramSocket socket;
+    private boolean running;
+    private byte[] buffer = new byte[256];
+
+    //TCP - DS-DS - Server
+    DatagramSocket serverSocket = new DatagramSocket(9876); //temp port
+    byte[] receiveData = new byte[1024];
+    byte[] sendData = new byte[1024];
+
+    //TCP - DS-DS - Client
+    DatagramSocket clientSocket = new DatagramSocket();
+    InetAddress IPAddress = InetAddress.getByName("Noahs-MacBook-Pro.local");
+    boolean serverOn = true;s
+
+    public DirectoryServer(int id) {
+        this.id = id;
+        //this.ip = ;
+        this.udpPort = 20650;
+        this.tcpPort = 20650;
+
+        //Setup UDP for Client-DS
+        this.socket = new DatagramSocket(this.udpPort);
+    }
+
+    public void linkDS(String child, String parent) {
+        this.predecessor = child;
+        this.sucessor = parent;
+    }
+
+    public void storeData(String contentName, int clientIP) {
+        int reponseCode = 200;
+        this.data.put(contentName, clientIP);
+
+        //do something with responseCode
+    }
+
+    public int getData(String contentName) {
+        int clientIp = this.data.get(contentName);
+        if (clientIp != -1) {
+            return clientIp;
+        } else {
+            return 404; // change to HTTP Response Codes
+        }
+    }
+
+    // UDP for Client-DS
+    public void run() {
+        running = true;
+
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+        
+        while (running) {
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+
+            InetAddress address = packet.getAddress();
+            int port = packet.getPort();
+            packet = new DatagramPacket(receiveData, receiveData.length, address, port); // return message
+
+            String request = new String(packet.getData(), 0, packet.getLength());
+
+            // evaluate request messages //init, get, update, exit
+            switch (request) {
+                case "init":
+                    break;
+
+                case "update":
+                    break;
+
+                case "query":
+                    break;
+                case "exit":
+                    running = false;
+                    continue;
+                    break;
+                default:
+                    //throw exception
+            }
+
+
+            socket.send(packet);
+        }
+        socket.close();
+    }
+
+    // TCP for DS-DS-Server
+    public void runServerTCP() {
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        serverSocket.receive(receivePacket);
+        String sentence = new String(receivePacket.getData());
+
+        InetAddress IPAddress = receivePacket.getAddress();
+
+        int port = receivePacket.getPort();
+
+        //String capitalizedSentence = sentence.toUpperCase();
+        //sendData = capitalizedSentence.getBytes();
+        String code = "200";
+        sendData = code.getBytes();
+
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+
+        serverSocket.send(sendPacket);
+    }
+
+    // TCP for DS-DS-Client
+    public void runClientTCP() {
+        while(serverOn)
+        {
+
+            BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+            byte[] sendData = new byte[1024];
+            byte[] receiveData = new byte[1024];
+
+            String sentence = inFromUser.readLine();
+            sendData = sentence.getBytes();
+
+            DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+            clientSocket.send(sendPacket);
+
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+            clientSocket.receive(receivePacket);
+
+            String result = new String(receivePacket.getData());
+
+            System.out.println("FROM SERVER:" + result);
+
+            if(sentence.toUpperCase().equals("EXIT"))
+            {
+                serverOn = false;
+            }
+
+        }
+        clientSocket.close();
+
+    }
+ 
+    public static void main(String args[])
+    {
+        //Instantiate variables
+        Scanner scanner = New Scanner(System.in);
+        int id = Integer.parseInt(args[0]);
+        DirectoryServer t = new DirectoryServer(id);
+
+        //Get Machine IP
+        InetAddress ip = InetAddress.getLocalHost();
+        String address = ip.getHostAddress(); //ip address of machine
+        System.out.println("Dir. Server - ID: " + id + " IP Address: " + address);
+
+        //Set DS ID#1 IP
+        System.out.println("Enter DS #1 IP: ");
+        t.primaryNode = in.nextLine();
+
+        //Set Neighbor IPs
+        System.out.println("Enter Child IP (space) Parent ID: ");
+        String[] neighbors = scanner.nextLine().split("\\s+");
+        t.linkDS(neighbors[0], neighbors[1]);
+
+        scanner.close();
+    }
+}
