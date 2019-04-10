f2d5d27c980dac61d3bab0053520139b59e17e8b
Judel Villardo <judel.isaac@live.ca>
2019-04-10 14:03:25 -0400

    Comments
    Comments

--- PCli.java
+++ PCli.java
@@ -2,25 +2,40 @@ import java.net.*;
 import java.io.*;
 import java.awt.image.*;
 import javax.imageio.*;
-
 import java.util.Scanner;
 
-// Peer-to-peer Client
+/**
+ * PCli, or Peer Client, connects to a Peer Server (local or otherwise)
+ * given the InetAddress (Name or literal IP), and port number.
+ *
+ * The user is given two options: [D]ownload or [E]xit
+ * If user selects [D]ownload, they are prompted to give the image file name
+ * that they wish to download. It will send a DOWNLOAD message to the Peer Server
+ * and the server will send the image.
+ * If user selects [E]xit, both Peer Client and Peer Server will shut down.
+ */
 public class PCli
-{
-    
+{ 
+    /**
+     * Main method of Peer Client
+     * @param   IP Address/Server Name of Peer Server (e.g. "localhost") to connect to
+     * @param   Port number of the Peer Server to connect to
+     */
     public static void main(String args[]) throws Exception
     {
-        InetAddress dst_srv = InetAddress.getByName(args[0]);
-        int srv_prt = Integer.parseInt(args[1]);
+        InetAddress dst_srv = InetAddress.getByName(args[0]);      // IP Address/Name
+        int srv_prt = Integer.parseInt(args[1]);                   // Server Port Number
         
         System.out.println("Connecting to " + dst_srv + ":" + srv_prt);
+        // Create Peer Client Socket connecting to Peer Server
         Socket cli = new Socket(dst_srv, srv_prt);
         System.out.println("Successfully connected to " + cli.getRemoteSocketAddress() + "\n");
         
+        // I/O Communication between Client and Server
         DataInputStream in = new DataInputStream(cli.getInputStream());     
         DataOutputStream out = new DataOutputStream(cli.getOutputStream());
         
+        // Reads user input
         Scanner read = new Scanner(System.in);
         String uInput;
         
@@ -28,22 +43,28 @@ public class PCli
         {
             System.out.println("Options: [D]ownload | [E]xit");
             System.out.print("Select option: ");
+            // Reads selection
             uInput = read.next();
             
             String uSel = uInput.toLowerCase();
             
             switch( uSel )
             {
+                // [D]ownload Image
                 case "d":
                     System.out.println("\nWhich image do you want to download?");
                     System.out.print("Input name: ");
+                        // Reads image file name that user client
                         uInput = read.next();
                     
+                    // Sends DOWNLOAD command to server alongside desired image
                     out.writeUTF("DOWNLOAD");
                     out.writeUTF(uInput);
                     
+                    // Reads image data given by Server
                     BufferedImage img = ImageIO.read(ImageIO.createImageInputStream(cli.getInputStream()));
                     
+                    // Writes and saves the image data into a folder
                     File download = new File("./img_c/" + uInput);
                     ImageIO.write(img, "jpg", download);
                 
@@ -51,7 +72,8 @@ public class PCli
                     cli.close();
                     System.exit(0);
                     break;
-                    
+                
+                // [E]xit both Peer Client and Peer Server
                 case "e":
                     System.out.println("\nShutting Down");
                     
@@ -60,7 +82,8 @@ public class PCli
                     cli.close();
                     System.exit(0);
                     break;
-                    
+                
+                // Any inputted command is invalid
                 default:
                     System.out.println("Invalid selection");
                     break;
--- PSrv.java
+++ PSrv.java
@@ -3,14 +3,30 @@ import java.io.*;
 import java.awt.image.*;
 import javax.imageio.*;
 
+/**
+ * PSrv, or Peer Server, connects to Peer Client (local or otherwise).
+ *
+ * It operates and waits for either two commands: DOWNLOAD or CLOSE.
+ * Upon receiving DOWNLOAD, it will read the desired image file and send it
+ * Upon receiving CLOSE, it will shut down alongside the Peer Client connected
+ */
 public class PSrv extends Thread
 {
     private ServerSocket sSocket;
     Socket srv;
     
+    /**
+     * Peer Server
+     * Creates the Server Socket for Peer Client to connect to and
+     * outputs the IP Address which others can use to connect
+     *
+     * @param   Port Number
+     */
     public PSrv(int prt) throws IOException, ClassNotFoundException, Exception
     {
+        // Create Server Socket for Peer Clients to connect to
         sSocket = new ServerSocket(prt);
+        // Print IP Address of running machine so others can connect to
         System.out.println
             (
             "Server IP Address is: " +
@@ -18,19 +34,27 @@ public class PSrv extends Thread
             );
     }
     
+    /**
+     * Runs thread waiting for either the DOWNLOAD or CLOSE command
+     * from the Peer Client
+     */
     public void run()
     {
         while (true)
         {
         try
         {
+            // Accept connection
             srv = sSocket.accept();
             
+            // I/O Communication Channels between Client and Server
             DataInputStream in = new DataInputStream(srv.getInputStream());
             DataOutputStream out = new DataOutputStream(srv.getOutputStream());
             
+            // Wait for Client Commmand
             switch (in.readUTF())
             {
+                // Recieves desired image and sends it to Client
                 case "DOWNLOAD":
                     System.out.println("Download Request Recieved");
                     String imgName = in.readUTF();
@@ -42,6 +66,7 @@ public class PSrv extends Thread
                     System.out.println( imgName + " sent to Client");
                     break;
                     
+                // Closes upon given the command
                 case "CLOSE":
                     System.out.println("Exit Command Received");
                     System.out.println("Shutting Down");
@@ -63,6 +88,12 @@ public class PSrv extends Thread
         }
     }
     
+    /**
+     * Main body of Peer Server
+     * Creates and starts thread 
+     *
+     * @param   Port Number
+     */
     public static void main(String args[]) throws IOException, ClassNotFoundException, Exception
     {
         Thread t = new PSrv( Integer.parseInt(args[0]));
